4f7599c3983b7dfae07925648b2a2f49821a8768
Quentin Derimais <quentin.derimais@gmail.com>
2026-06-22 15:41:51 +0200

    feat(backend): add article and feed modules to app imports

--- services/backend/src/app.module.ts
+++ services/backend/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { ArticleModule } from "./article/article.module";
+import { FeedModule } from "./feed/feed.module";
 
 @Module({
-	imports: [],
+	imports: [FeedModule, ArticleModule],
 	controllers: [AppController],
 	providers: [AppService],
 })
