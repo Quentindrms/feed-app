bf3dd732a37e11cb8bf7694c0a2c913c8895e210
Quentin Derimais <quentin.derimais@gmail.com>
2026-06-22 15:41:31 +0200

    feat(api): add article and feed modules with basic controllers

--- services/backend/src/app.controller.ts
+++ services/backend/src/app.controller.ts
@@ -9,4 +9,10 @@ export class AppController {
 	getHello(): string {
 		return this.appService.getHello();
 	}
+
+	@Get("test")
+	async test() {
+		const feed = await this.appService.getTest();
+		return { feed };
+	}
 }
