4c25b66dc2d760a66494442f43ae9fd12804421a
Quentin Derimais <quentin.derimais@gmail.com>
2026-06-25 15:41:14 +0200

    refactor(card): improve layout structure for publication date

--- services/frontend/src/components/card.tsx
+++ services/frontend/src/components/card.tsx
@@ -13,20 +13,22 @@ export default function Card(props: CardProps) {
 	const publicationDate = new Date(props.publicationDate);
 
 	return (
-		<div className="flex flex-col gap-3 shadow-md shadow-dark/10 bg-muted-background/30 border border-dark-border/10 w-md h-65 rounded-2xl p-3 hover:scale-101 transition-transform duration-150">
+		<div className="flex flex-col justify-between gap-3 shadow-md shadow-dark/10 bg-muted-background/30 border border-dark-border/10 w-md h-65 rounded-2xl p-3 hover:scale-101 transition-transform duration-150">
 			<Heading component="h2" size="xl">
 				{props.title}
 			</Heading>
 			<Text component="p" size="md" weight="light">
 				{props.description}
 			</Text>
-			<Text component="p" size="md" weight="extralight">
-				Date de publication :{" "}
-				<Text component="span" size="md" weight="light">
-					{publicationDate.toLocaleDateString("fr-FR")}
+			<div className="flex justify-between">
+				<Text component="p" size="md" weight="extralight">
+					Date de publication :{" "}
+					<Text component="span" size="md" weight="light">
+						{publicationDate.toLocaleDateString("fr-FR")}
+					</Text>
 				</Text>
-			</Text>
-			<Link href={props.link}>{"Consulter l'article"}</Link>
+				<Link href={props.link}>{"Consulter l'article"}</Link>
+			</div>
 		</div>
 	);
 }
