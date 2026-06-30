9582421afef94001f2947e7e9a88fb80077e016b
Quentin Derimais <quentin.derimais@gmail.com>
2026-06-30 23:45:49 +0200

    feat(backend): add reddit username validation

--- services/backend/src/main.ts
+++ services/backend/src/main.ts
@@ -1,9 +1,15 @@
+import { Logger } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
+	const logger = new Logger();
 	const app = await NestFactory.create(AppModule);
 	app.enableCors();
+	if (!process.env.REDDIT_USERNAME) {
+		logger.error("Reddit username is missing");
+		throw new Error("Reddit username is missing");
+	}
 	await app.listen(process.env.BACKEND_PORT ?? 4000);
 }
 bootstrap();
