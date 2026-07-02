1e8aba83cc3d84b046fd2b830f03f15f0f595d04
Quentin Derimais <quentin.derimais@gmail.com>
2026-07-02 15:10:17 +0200

    feat(hooks): add article count and pagination utilities

--- services/frontend/src/hooks/useArticle.ts
+++ services/frontend/src/hooks/useArticle.ts
@@ -5,11 +5,22 @@ export default function useArticle() {
 	const fetcher = new Fetcher();
 
 	async function browseArticle() {
-		const article = await fetcher.get<Article[]>("article");
+		const article = await fetcher.get<Article[]>(`article`);
 		return article;
+	}
+
+	async function countArticle() {
+		const total = await fetcher.get("article/count");
+		return total;
+	}
+
+	function countPageNumber(totalArticle: number, pagination: number) {
+		return Array.from({ length: Math.ceil(totalArticle / pagination) }, (_, i) => i + 1);
 	}
 
 	return {
 		browseArticle,
+		countArticle,
+		countPageNumber,
 	};
 }
