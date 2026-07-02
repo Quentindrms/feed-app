a9c1f010204607f0e3827a553687195dd263342d
Quentin Derimais <quentin.derimais@gmail.com>
2026-07-02 15:09:46 +0200

    feat(ui): add pagination component and home page pagination

--- services/frontend/src/pages/index/home.tsx
+++ services/frontend/src/pages/index/home.tsx
@@ -1,3 +1,4 @@
+import { PaginationButton } from "@src/components/button";
 import Card from "@src/components/card";
 import NavBar from "@src/components/navBar";
 import useArticle from "@src/hooks/useArticle";
@@ -8,10 +9,18 @@ export default function Home() {
 	const article = useArticle();
 
 	const [articleList, setArticleList] = useState<Article[]>([]);
+	const [count, setCount] = useState(0);
+	const [pages, setPages] = useState<Array<number>>([]);
+	const [currentPage, setCurrentPage] = useState(1);
 
 	useEffect(() => {
 		article.browseArticle().then(setArticleList);
+		article.countArticle().then(setCount);
 	}, []);
+
+	useEffect(() => {
+		setPages(article.countPageNumber(count, 10));
+	}, [count]);
 
 	return (
 		<div className="bg-light-background min-h-dvh">
@@ -29,6 +38,22 @@ export default function Home() {
 						/>
 					))}
 				</div>
+			</div>
+			<div className="flex gap-2 justify-center items-center">
+				{pages.slice(0, 5).map((page) => (
+					<PaginationButton
+						key={page}
+						number={page}
+						onClick={() => setCurrentPage(page)}
+					/>
+				))}
+				{pages.length > 6 && <span>...</span>}
+				{pages.length > 5 && (
+					<PaginationButton
+						number={pages[pages.length - 1]}
+						onClick={() => setCurrentPage(pages[pages.length - 1])}
+					/>
+				)}
 			</div>
 		</div>
 	);
